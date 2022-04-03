f09901743d76bed687f2dc9dd84968e8b69898c5
FilipeSouza <filipe.souza1602@gmail.com>
2022-04-03 14:46:47 -0300

    feat: implement youtube iframe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'teste-iframe';
+export class AppComponent implements OnInit {
+
+  player: any;
+
+  ngOnInit(): void {
+    this.loadIframeApi();
+  }
+
+  loadIframeApi() {
+    const tag = document.createElement('script');
+    tag.src = "http://www.youtube.com/player_api";
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+    window['onYouTubeIframeAPIReady'] = () => this.startVideo();
+  }
+
+  startVideo() {
+    this.player = new window['YT'].Player('player', {
+      videoId: 'xdV_bzJgRxE',
+      events: {
+        'onReady': this.onPlayerReady.bind(this),
+        'onStateChange': this.onPlayerStateChange.bind(this)
+      }
+    });
+  }
+
+  onPlayerReady(event) {
+    console.log('onPlayerReady: ',event);
+  }
+
+  onPlayerStateChange(event) {
+    console.log('onPlayerStateChange: ',event);
+  }
 }
